77f819eb9eb7047bd8d234ea300c20eb07a9367a
Vu Truong Anh <truonganhvu205@gmail.com>
2025-10-25 09:14:25 +0700

    patch

--- src/app/configs/env.config.js
+++ src/app/configs/env.config.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const configEnv = {
     serverPort: process.env.SERVER_PORT,
     authPort: process.env.AUTH_PORT,
+    adminUser: process.env.ADMIN_USERNAME,
+    adminPassword: process.env.ADMIN_PASSWORD,
     accessTokenSecretKey: process.env.ACCESS_TOKEN_SECRET_KEY,
     refreshTokenSecretKey: process.env.REFRESH_TOKEN_SECRET_KEY,
 }
--- src/app/databases/auth/roles.database.js
+++ src/app/databases/auth/roles.database.js
@@ -1,6 +1,7 @@
 const rolesDb = {
     admin: 0,
     user: 1,
+    // guest: 2,
 }
 
 module.exports = rolesDb
